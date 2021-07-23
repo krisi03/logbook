16a7c898ad574534877c76dff89b83951e5f1bf7
jordan8187 <80174945+jordan8187@users.noreply.github.com>
2021-07-23 23:47:22 +0300

    Visual changes.

--- logBook/logBook/Form1.Designer.cs
+++ logBook/logBook/Form1.Designer.cs
@@ -44,32 +44,34 @@ namespace logBook
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Stencil", 25.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(358, 54);
+            this.label1.Location = new System.Drawing.Point(321, 40);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(210, 50);
+            this.label1.Size = new System.Drawing.Size(170, 40);
             this.label1.TabIndex = 1;
             this.label1.Text = "logbook";
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.BackColor = System.Drawing.Color.DarkCyan;
             this.panel1.Controls.Add(this.register);
             this.panel1.Controls.Add(this.SignIn);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Location = new System.Drawing.Point(-1, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(945, 576);
+            this.panel1.Size = new System.Drawing.Size(827, 432);
             this.panel1.TabIndex = 1;
             // 
             // register
             // 
             this.register.BackColor = System.Drawing.Color.White;
-            this.register.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.register.Font = new System.Drawing.Font("Segoe UI", 12.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.register.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.register.Location = new System.Drawing.Point(358, 447);
+            this.register.Location = new System.Drawing.Point(322, 321);
+            this.register.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.register.Name = "register";
-            this.register.Size = new System.Drawing.Size(189, 49);
+            this.register.Size = new System.Drawing.Size(170, 44);
             this.register.TabIndex = 3;
             this.register.Text = "Register";
             this.register.UseVisualStyleBackColor = false;
@@ -77,11 +79,12 @@ namespace logBook
             // SignIn
             // 
             this.SignIn.BackColor = System.Drawing.Color.White;
-            this.SignIn.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.SignIn.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.SignIn.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.SignIn.Location = new System.Drawing.Point(358, 347);
+            this.SignIn.Location = new System.Drawing.Point(322, 250);
+            this.SignIn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.SignIn.Name = "SignIn";
-            this.SignIn.Size = new System.Drawing.Size(189, 49);
+            this.SignIn.Size = new System.Drawing.Size(170, 44);
             this.SignIn.TabIndex = 3;
             this.SignIn.Text = "Sign In";
             this.SignIn.UseVisualStyleBackColor = false;
@@ -90,18 +93,20 @@ namespace logBook
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(324, 137);
+            this.pictureBox1.Location = new System.Drawing.Point(283, 103);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(276, 133);
+            this.pictureBox1.Size = new System.Drawing.Size(250, 102);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(944, 575);
+            this.ClientSize = new System.Drawing.Size(826, 431);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
